00db549159b1339457570df36787f6aa0598d73e
ArdaKaymaz <146623362+ArdaKaymaz@users.noreply.github.com>
2025-07-03 01:44:56 +0300

    refactor: models file added
    In order to design a clear and moduler architecture, created a file that contains Pydantic data models (models.py)

---
+++ models.py
@@ -0,0 +1,28 @@
+from pydantic import BaseModel, Field
+from typing import List, Optional, Dict
+
+class Chunk(BaseModel):
+    text: str
+    source_doc_id: str
+    section_heading: str
+    journal: str
+    publish_year: int
+    attributes: Dict[str, str] = Field(default_factory=dict)
+
+class UploadRequest(BaseModel):
+    chunks: List[Chunk]
+
+class SimilaritySearchRequest(BaseModel):
+    query: str
+    k: int = 10
+    min_score: float = 0.25
+
+class SearchResult(BaseModel):
+    id: str
+    text: str
+    score: float
+    source_doc_id: str
+    section_heading: str
+    journal: str
+    publish_year: int
+    attributes: Dict[str, str]
