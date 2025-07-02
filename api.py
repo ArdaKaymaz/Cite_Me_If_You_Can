8d5b80992f05b4dd72dbc1110bba2a9d90cfeec6
ArdaKaymaz <146623362+ArdaKaymaz@users.noreply.github.com>
2025-07-03 00:28:49 +0300

    feat (models): add new data models for FastAPI
    - Added chunk,
    - Added upload request,
    - Added similarity search request,
    - Added search result
    data models

--- api.py
+++ api.py
@@ -9,3 +9,30 @@ app = FastAPI()
 
 # Temporary vector db
 vector_store = []
+
+# Data models
+class Chunk(BaseModel):
+    text: str
+    source_doc_id: str
+    section_heading: str
+    journal: str
+    publish_year: int
+    attributes: Optional[Dict[str, str]] = Field(default_factory=dict)
+
+class UploadRequest(BaseModel):
+    chunks: List[Chunk]
+
+class SimilaritySearchRequest(BaseModel):
+    query: str
+    k: int = 10
+    min_score: float = 0.25
+
+class SearchResult(BaseModel):
+    id: str
+    text: str
+    score: float
+    source_doc_id: str
+    section_heading: str
+    journal: str
+    publish_year: int
+    attributes: Optional[Dict[str, str]]
